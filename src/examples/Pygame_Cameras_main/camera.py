86a141e6e37c24e7b957a893b732bf5ad5f940f5
Виталий Павлюков <aikimind@mail.ru>
2024-10-26 15:14:25 +0300

    add functions

--- src/examples/Pygame_Cameras_main/camera.py
+++ src/examples/Pygame_Cameras_main/camera.py
@@ -3,6 +3,7 @@ import sys
 import os
 from random import randint
 from examples.Pygame_Cameras_main.tree import Tree
+from examples.Pygame_Cameras_main.house import House
 from examples.Pygame_Cameras_main.player import Player
 from examples.Pygame_Cameras_main.worker import Worker
 from examples.Pygame_Cameras_main.camera_group import CameraGroup
@@ -18,8 +19,8 @@ class CameraGame:
 
 		# setup
 		self.camera_group = CameraGroup(base_path=self.base_path)
-		self.player = Player((640, 360), self.camera_group, base_path=self.base_path)
-		self.worker = Worker((640, 450), self.camera_group, base_path=self.base_path)
+		self.player = Player((1000, 1000), self.camera_group, base_path=self.base_path)
+		self.worker = Worker((1000, 1150), self.camera_group, base_path=self.base_path)
 
 		self.tree_list = []
 		for i in range(20):
@@ -29,6 +30,10 @@ class CameraGame:
 			tree.name = tree.name + ' ' + str(i+1)
 			self.tree_list.append(tree)
 
+		self.house = House((640, 1000), self.camera_group, base_path=self.base_path)
+
+		self.camera_group.offset = (210, 785)
+
 	def set_running(self, value):
 		self.running = value
 
@@ -36,6 +41,25 @@ class CameraGame:
 			pygame.event.set_grab(True)
 		else:
 			pygame.event.set_grab(False)
+
+	def select_objects(self, value):
+		self.house.mouse_selected = value
+		self.worker.mouse_selected = value
+
+		if not value:
+			self.camera_group.tree_selected_index = -1
+
+	def get_selected_objects(self):
+		if self.house.mouse_selected:
+			return 'house'
+
+		if self.worker.mouse_selected:
+			return 'worker'
+
+		if self.camera_group.tree_selected_index >= 0:
+			return 'tree'
+
+		return None
 
 	def run(self, events):
 		if not self.running:
@@ -53,41 +77,73 @@ class CameraGame:
 			if event.type == pygame.MOUSEWHEEL:
 				self.camera_group.zoom_scale += event.y * 0.03
 
+			if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+				if self.get_selected_objects():
+					self.select_objects(False)
+
 		self.screen.fill('#71ddee')
 
 		self.camera_group.update()
-		self.camera_group.custom_draw(self.player, self.worker, self.tree_list)
+		self.camera_group.custom_draw(events, self.player, self.worker, self.tree_list, self.house)
 
 		# Панель управления
-		# self.display_surface.blit(scaled_surf, scaled_rect)
 		BLACK = (0, 0, 0)
 		panel_surf = pygame.Surface((400, 800))
-		pygame.draw.rect(panel_surf, BLACK,
-						 pygame.Rect(0, 0, 400, 800))
+		pygame.draw.rect(panel_surf, BLACK, pygame.Rect(0, 0, 400, 800))
 
-		# Статистика
-		stats = [
-				('Статистика', ''),
-				('player.rect', self.player.rect),
-				('camera_group.offset', self.camera_group.offset),
-				('camera_group.camera_rect', self.camera_group.camera_rect),
-				('test_stat', self.camera_group.test_stat),
-				('zoom_scale', self.camera_group.zoom_scale),
-				('len(tree_list)', len(self.tree_list)),
-				('test_ground_offset', self.camera_group.test_ground_offset),
+		p_selected_object = self.get_selected_objects()
+		stats = []
+
+		if not p_selected_object:
+			# Статистика
+			stats = [
+					('Статистика', ''),
+					('player.rect', self.player.rect),
+					('camera_group.offset', self.camera_group.offset),
+					('camera_group.camera_rect', self.camera_group.camera_rect),
+					('test_stat', self.camera_group.test_stat),
+					('zoom_scale', self.camera_group.zoom_scale),
+					('len(tree_list)', len(self.tree_list)),
+					('test_ground_offset', self.camera_group.test_ground_offset),
+					('test_tree', self.camera_group.test_tree),
+					('pygame.mouse.get_pos()', pygame.mouse.get_pos()),
+					('test_scaled_tree_rect', self.camera_group.test_scaled_tree_rect),
+					('test_tree_selected_index', self.camera_group.test_tree_selected_index),
+					('test_worker', self.camera_group.test_worker),
+					('test_house', self.camera_group.test_house),
+					('Собрано деревьев', self.camera_group.test_tree_done_count),
+					('Собрано деревьев в доме', self.house.done_tree_count)
+					]
+
+		elif p_selected_object == 'house':
+			# Статистика
+			stats = [
+				('Дом', ''),
+				('Собрано деревьев в доме', self.house.done_tree_count)
+			]
+
+		elif p_selected_object == 'worker':
+			# Статистика
+			stats = [
+				('Рабочий', ''),
+				('test_worker', self.camera_group.test_worker)
+			]
+
+		elif p_selected_object == 'tree':
+			# Статистика
+			stats = [
+				('Дерево', ''),
 				('test_tree', self.camera_group.test_tree),
-				('pygame.mouse.get_pos()', pygame.mouse.get_pos()),
-				('test_scaled_tree_rect', self.camera_group.test_scaled_tree_rect),
-				('test_tree_selected_index', self.camera_group.test_tree_selected_index),
-				('test_worker', self.camera_group.test_worker),
 				('Собрано деревьев', self.camera_group.test_tree_done_count)
-				]
+			]
+
 		for i, line in enumerate(stats):
 			font = pygame.font.Font(None, 24)
 			surf_text = font.render(str(line[0]) + ': ' + str(line[1]), True, (255, 255, 255))
-			panel_surf.blit(surf_text, (10, 300 + (i*30)))
+			panel_surf.blit(surf_text, (10, 300 + (i * 30)))
 
-		scaled_surf = pygame.transform.scale(self.camera_group.internal_surf, self.camera_group.internal_surface_size_vector * 0.1)
+		scaled_surf = pygame.transform.scale(self.camera_group.internal_surf,
+											 self.camera_group.internal_surface_size_vector * 0.1)
 		panel_surf.blit(scaled_surf, (0, 0))
 
 		self.screen.blit(panel_surf, (0, 0))
---
+++ src/examples/Pygame_Cameras_main/house.py
@@ -0,0 +1,27 @@
+import os
+import pygame
+
+
+class House(pygame.sprite.Sprite):
+	def __init__(self, pos, group, base_path=''):
+		super().__init__(group)
+		self.image = pygame.image.load(os.path.join(base_path, 'graphics/Summer-Medieval-City-2D-Tileset2.png')).convert_alpha()
+		self.image = self.clip(self.image, 165, 0, 308, 326)
+		self.image = pygame.transform.scale(self.image, (308, 326))
+		self.image.set_colorkey((47, 50, 47))
+
+		self.rect = self.image.get_rect(topleft=pos)
+		self.name = 'Дом'
+		self.size = 1
+		self.done_tree_count = 0
+		self.mouse_selected = False
+
+	def transform(self):
+		self.image = pygame.transform.scale(self.image, (50, 50))
+
+	def clip(self, surface, x, y, x_size, y_size):  # Get a part of the image
+		handle_surface = surface.copy()  # Sprite that will get process later
+		clipRect = pygame.Rect(x, y, x_size, y_size)  # Part of the image
+		handle_surface.set_clip(clipRect)  # Clip or you can call cropped
+		image = surface.subsurface(handle_surface.get_clip())  # Get subsurface
+		return image.copy()  # Return
